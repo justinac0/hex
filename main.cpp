168dcf543eb937aa97502c09fc0e4a942076dba1
justinac0 <justinac.1999@gmail.com>
2025-06-04 20:06:53 +1000

    added camera move and scroll, added more tiles

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
-
+#include <assert.h>
 #include "util.hpp"
 #include "tile.hpp"
 
 #define SCREEN_WIDTH (1280)
 #define SCREEN_HEIGHT (720)
+
+Vector2 Vector2Scale(Vector2 a, float scalar) {
+    return {
+        a.x * scalar,
+        a.y * scalar,
+    };
+}
+
+Vector2 Vector2Add(Vector2 a, Vector2 b) {
+    return {
+        a.x + b.x,
+        a.y + b.y,
+    };
+}
+
+float Clamp(float value, float min, float max) {
+    const float t = value < min ? min : value;
+    return t > max ? max : t;
+}
+
+void UpdateCamera(Camera2D *camera) {
+    assert(camera);
+
+    // move
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+        Vector2 delta = GetMouseDelta();
+        delta = Vector2Scale(delta, -1.0f/camera->zoom);
+        camera->target = Vector2Add(camera->target, delta);
+    }
+
+    // scroll
+    float wheel = GetMouseWheelMove();
+    if (wheel != 0) {
+        Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), *camera);
+        camera->offset = GetMousePosition();
+        
+        camera->target = mouseWorldPos;
+        float scale = 0.2f*wheel;
+        camera->zoom = Clamp(expf(logf(camera->zoom) + scale), 0.125f, 64.0f);
+    }
+}
 
 int main() {
     srand(time(NULL));
@@ -13,26 +54,24 @@ int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "hex");
 
     // setup camera
-    Camera2D camera;
-    // camera.offset = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
-    camera.target = {0.0f, 0.0f};
-    camera.rotation = 0.0f;
+    Camera2D camera = {};
     camera.zoom = 1.0f;
 
     // setup grid
-    Tile grid[100];
+    Tile grid[10000];
     int i = 0;
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
+    for (int y = 0; y < 100; y++) {
+        for (int x = 0; x < 100; x++) {
             grid[i++] = Tile({x, y});
         }
     }
    
     // draw, update
     while (!WindowShouldClose()) {
+        UpdateCamera(&camera);
+
         BeginDrawing();
         ClearBackground(BLACK);
-
         BeginMode2D(camera);
 
         Vector2 mp = GetMousePosition();
@@ -47,6 +86,11 @@ int main() {
             t.DebugDraw();
         }
         #endif
+
+        if (IsKeyPressed(KEY_R)) {
+            camera = {};
+            camera.zoom = 1.0f;
+        }
 
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || true) {
             for (auto &t : grid) {
