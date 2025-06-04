cbb0544be931a0f4cfb4d5adea0e50bf59495b40
justinac0 <justinac.1999@gmail.com>
2025-06-04 18:46:11 +1000

    centered debug text with opaque bg

--- tile.cpp
+++ tile.cpp
@@ -22,21 +22,25 @@ void Tile::Draw() {
     DrawPoly({x_offset, y_offset}, 6, TILE_RADIUS, 0, this->color);
 }
 
-#define TILE_TEXT_SIZE 48
 void Tile::DebugDraw() {
+#define TILE_TEXT_SIZE (48)
+#define TILE_TEXT_PADDING (8)
     float tessellation_width = TILE_RADIUS + TILE_RADIUS * 0.5; // r + r*cos(pi/3); the axis-aligned width + the slanted side width
     float tessellation_height = TILE_RADIUS * sinf(PI/3) * 2; // r*sin(pi/3)*2; the height slanted side height times 2;
     bool is_odd_column = (this->pos.x % 2);
     float x_offset = this->pos.x * tessellation_width;
     float y_offset = this->pos.y * tessellation_height + (is_odd_column * tessellation_height * 0.5);
-    
-    char buff[256];
-    int status = sprintf(buff, "%d, %d", pos.x, pos.y);
-    if (status > 0) buff[status + 1] = '\0';
-    int length = TextLength(buff);
 
-    float text_x_offset = TILE_TEXT_SIZE * 0.5 * length; // center text horizontally
-    float text_y_offset = -TILE_TEXT_SIZE * 0.5; // center text vertically
-    DrawText(buff, x_offset - (length * , y_offset - TILE_TEXT_SIZE / 2, TILE_TEXT_SIZE, WHITE);
-}
+    // tile position to string
+    char buff[256];
+    int nbytes = sprintf(buff, "%d, %d", pos.x, pos.y);
+    if (nbytes > 0) buff[nbytes + 1] = '\0';
+
+    float text_width = MeasureText(buff, TILE_TEXT_SIZE);
+    float text_x_offset = text_width * 0.5; // center text horizontally
+    float text_y_offset = TILE_TEXT_SIZE * 0.5; // center text vertically
+    DrawRectangle(x_offset - text_x_offset - TILE_TEXT_PADDING, y_offset - text_y_offset - TILE_TEXT_PADDING, text_width + TILE_TEXT_PADDING * 2, TILE_TEXT_SIZE + TILE_TEXT_PADDING * 2, Fade(BLACK, 0.5f));
+    DrawText(buff, x_offset - text_x_offset, y_offset - text_y_offset, TILE_TEXT_SIZE, WHITE);
 #undef TILE_TEXT_SIZE
+#undef TILE_TEXT_PADDING
+}
